ac14df4f5e89e7ec8d86a634d9c4fd1d919687e2
AJMANZ <113150864+AJManzione@users.noreply.github.com>
2022-10-28 11:33:19 -0600

    path's working

--- server.js
+++ server.js
@@ -3,11 +3,38 @@
 const express = require('express');
 const path = require('path');
 
+let db = require('./db/db.json');
 // Sets up the Express App
 
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
+
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
+
+app.get('/api/notes', (req, res) => {
+    res.json(db)
+});
+
+app.post('/api/notes', (req, res) => {
+
+});
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () =>  console.info(`Example app listening at http://localhost:${PORT} 🚀`));
