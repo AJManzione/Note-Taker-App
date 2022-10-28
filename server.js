15c19d0ea20cd915a94a5ad421768d208b2a2924
AJMANZ <113150864+AJManzione@users.noreply.github.com>
2022-10-28 14:37:49 -0600

    all paths get and post declared

--- server.js
+++ server.js
@@ -1,64 +1,31 @@
-// Dependencies
+const PORT = process.env.PORT || 3001;
+const fs = require('fs');
+const path = require('path');
 
 const express = require('express');
-const path = require('path');
-const fs = require('fs')
-
-let db = require('./db/db.json');
-const { randomUUID } = require('crypto');
-// Sets up the Express App
-
 const app = express();
-const PORT = 3001;
 
-// Sets up the Express app to handle data parsing
+const db = require('./db/db.json');
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
-
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
-
-app.post('/api/notes', (req, res) => {
-    let newNote;
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-        if (err) {
-            console.error(err);
-        } else {
-            const detNotes = JSON.parse(data);
-            req.body.id = randomUUID;
-            detNotes.push(req.body); 
-
-
-    fs.writeFile('./db/db.json', 
-    JSON.stringify(detNotes, null, 4), 
-    (err) => {
-        err
-        ? console.error(err)
-        : console.info('successfully updated notes!')   
-    });
-
-    db = detNotes;
-    res.json(req.body)
-    res.sendFile(path.join(__dirname, + '/public' + 'api/notes.html'))
-    }
-})
-
-});
-
 app.get('/api/notes', (req, res) => {
-    res.json(db)
+    res.json(db.slice(1));
 });
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
 
-
-
-
-
-
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 
 
