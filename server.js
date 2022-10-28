d49b63ecd8db6504139d5ca52274140da7833b83
AJMANZ <113150864+AJManzione@users.noreply.github.com>
2022-10-28 11:52:59 -0600

    updating the db - need to get it to live print results

--- server.js
+++ server.js
@@ -2,8 +2,10 @@
 
 const express = require('express');
 const path = require('path');
+const fs = require('fs')
 
 let db = require('./db/db.json');
+const { randomUUID } = require('crypto');
 // Sets up the Express App
 
 const app = express();
@@ -19,12 +21,35 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html
 
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
 
-app.get('/api/notes', (req, res) => {
-    res.json(db)
+app.post('/api/notes', (req, res) => {
+    let newNote;
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            const detNotes = JSON.parse(data);
+            req.body.id = randomUUID;
+            detNotes.push(req.body); 
+
+
+    fs.writeFile('./db/db.json', 
+    JSON.stringify(detNotes, null, 4), 
+    (err) => {
+        err
+        ? console.error(err)
+        : console.info('successfully updated notes!')   
+    });
+
+    db = newNote;
+    res.json(req.body)
+    res.sendFile(path.join(__dirname, + '/public' + 'api/notes.html'))
+    }
+})
+
 });
 
-app.post('/api/notes', (req, res) => {
-
+app.get('/api/notes', (req, res) => {
+    res.json(db)
 });
 
 
